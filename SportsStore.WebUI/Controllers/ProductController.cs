e53cbfde438a91905297b01de40c286ac40d562e
vanhineam <vanhineam@appstate.edu>
2014-01-24 10:34:25 -0500

    All Good

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -7,8 +7,6 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Models;
 
-//Z Testing a change
-
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
