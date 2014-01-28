f58ff0a44a8e4a3f676a7439f94b795375903fac
vanhineam <vanhineam@appstate.edu>
2014-01-28 12:49:46 -0500

    Highlighting Categories

--- SportsStore.UnitTests/Tests/UnitTest1.cs
+++ SportsStore.UnitTests/Tests/UnitTest1.cs
@@ -85,7 +85,7 @@ namespace SportsStore.UnitTests.Tests
             controller.PageSize = 3;
 
             // Act
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
 
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
@@ -121,6 +121,56 @@ namespace SportsStore.UnitTests.Tests
             Assert.AreEqual(result.Length, 2);
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // - can the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            }.AsQueryable());
+
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+
+            // Act - get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            // Arrange
+            // - create the mock repo
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product {ProductID = 2, Name = "P2", Category = "Oranges" },
+            }.AsQueryable());
+
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+
+            // Arrange - define the category to be selected
+            string categoryToSelect = "Apples";
+
+            // Action
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+            // Assert
+            Assert.AreEqual(categoryToSelect, result);
         }
     }
 }
